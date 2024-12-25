9cc2e7ddf558c97fdaf4760babebad13d32d62a9
qwe7002-dev <qwe7002.dev@gmail.com>
2024-12-25 13:56:39 +0800

    fix bark

--- src/carbon_copy/bark.tsx
+++ src/carbon_copy/bark.tsx
@@ -1,52 +1,73 @@
-import {Alert, Button, TextField} from "@mui/material";
+import {Alert, Autocomplete, Button, FormControlLabel, Switch, TextField} from "@mui/material";
 import React, {useState} from "react";
 import {useQrious} from "react-qrious";
 
 function Bark() {
-    const [formData, setFormData] = React.useState({
-            method: 0,
-            webhook: "",
-            body: "",
-            enabled: true,
-            header: ""
-        }
-    );
     const [server, setServer] = useState("");
-    const [value, setValue] = useState('');
+    const [icon, setIcon] = useState("https://avatars.githubusercontent.com/u/50076056?s=128&v=4");
+    const [value, setValue] = useState("");
     const [qrCode, _qrious] = useQrious({value, size: 512, padding: 20, mime: 'image/png'});
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const {host, key} = extractHostAndKey(server);
-        setFormData({
-            ...formData,
-            webhook: `https://${host}/${key}/{{Title}}/{{Message}}?copy={{Copy}}`
-        });
-        setValue(JSON.stringify(formData));
-        console.log(formData);
-    };
+    const barkSounds = [
+        "alarm", "anticipate", "bell", "birdsong", "bloom", "calypso", "chime", "choo", "descent", "electronic", "fanfare", "gathering", "glass", "gotosleep", "healthnotification", "horn", "ladder", "mailsent", "minuet", "multiwayinvitation", "newmail", "newsflash", "noir", "paymentsuccess", "shake", "sherwoodforest", "silence", "spell", "suspense", "telegraph", "tiptoes", "typewriters", "update", "uplift", "voicemail"
+    ];
+    const [useRingtone, setUseRingtone] = useState(false);
+    const [ringtone, setRingtone] = useState("alarm");
     return (
-        <form onSubmit={handleSubmit}>
+        <>
             <div>
                 <TextField type="text"
-                           value={server} onChange={(event)=>{
+                           value={server} onChange={(event) => {
                     setServer(event.target.value);
                 }} label="Webhook URL"
                            variant="outlined" required/>
+                <TextField type="text" value={icon} onChange={(event) => {
+                    setIcon(event.target.value);
+                }} label="Icon URL" variant="outlined" required/>
+                <Button type="submit" onClick={(event) => {
+                    event.preventDefault();
+                    const {host, key} = extractHostAndKey(server);
+                    const formData = {
+                        method: 0,
+                        webhook: "",
+                        body: "",
+                        enabled: true,
+                        header: ""
+                    }
+                    formData.webhook = `https://${host}/${key}/{{Title}}/{{Message}}?copy={{Copy}}&icon=${encodeURIComponent(icon)}`;
+                    if (useRingtone) {
+                        formData.webhook += `&sound=${ringtone}`;
+                    }
+                    const Json = JSON.stringify(formData)
+                    setValue(Json);
+                }} variant="contained">Generate QR Code</Button>
             </div>
             <div>
-                <Button type="submit" variant="contained">Generate QR Code</Button>
+                <FormControlLabel control={<Switch name="fallback_sms"
+                                                   checked={useRingtone}
+                                                   onChange={()=>{
+                                                       useRingtone ? setUseRingtone(false) : setUseRingtone(true);
+                                                   }}/>}
+                                  label="Using RingTone"/>
+                <Autocomplete
+                    style={{display: useRingtone ? 'block' : 'none'}}
+                    freeSolo
+                    options={barkSounds}
+                    value={ringtone}
+                    onChange={(event, newValue) => {setRingtone(newValue?newValue:"alarm")}}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Sounds" />}
+                />
             </div>
             <div style={{display: value ? 'none' : 'block'}}>
                 <Alert variant="filled" severity="info">
-                    our generated QR Code will be displayed here
+                    Your generated QR Code will be displayed here
                 </Alert>
             </div>
             <div style={{display: value ? 'block' : 'none'}}>
                 <img src={qrCode} alt="QR Code"/>
             </div>
-        </form>
-    )
-        ;
+        </>
+    );
 }
 
 function extractHostAndKey(url: string) {
--- src/carbon_copy/lark.tsx
+++ src/carbon_copy/lark.tsx
@@ -3,14 +3,13 @@ import React, {useState} from "react";
 import {useQrious} from "react-qrious";
 
 function Lark() {
-    const [formData, setFormData] = React.useState({
+    const formData = {
             method: 1,
             webhook: "",
             body: "",
             enabled: true,
             header: ""
-        }
-    );
+        };
 
     const body = {
         msg_type: "post",
@@ -33,27 +32,21 @@ function Lark() {
     const [server, setServer] = useState("");
     const [value, setValue] = useState('');
     const [qrCode, _qrious] = useQrious({value, size: 512, padding: 20, mime: 'image/png'});
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setFormData({
-            ...formData,
-            webhook: server,
-            body: JSON.stringify(body)
-        });
-        setValue(JSON.stringify(formData));
-        console.log(formData);
-    };
     return (
-        <form onSubmit={handleSubmit}>
+        <>
             <div>
                 <TextField type="text"
                            value={server} onChange={(event) => {
                     setServer(event.target.value);
                 }} label="Webhook URL"
                            variant="outlined" required/>
-            </div>
-            <div>
-                <Button type="submit" variant="contained">Generate QR Code</Button>
+                <Button type="submit" onClick={(event) => {
+                    event.preventDefault();
+                    formData.webhook = server;
+                    formData.body = JSON.stringify(body);
+                    setValue(JSON.stringify(formData));
+                    console.log(formData);
+                }} variant="contained">Generate QR Code</Button>
             </div>
             <div style={{display: value ? 'none' : 'block'}}>
                 <Alert variant="filled" severity="info">
@@ -63,7 +56,7 @@ function Lark() {
             <div style={{display: value ? 'block' : 'none'}}>
                 <img src={qrCode} alt="QR Code"/>
             </div>
-        </form>
+        </>
     );
 }
 
