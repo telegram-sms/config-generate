6e9453699709418a9177e4487b57c313c776c760
ous50 <36183551+ous50@users.noreply.github.com>
2025-01-07 03:47:05 +0800

    Auto Dark mode (#5D576B temporarily dumped)

--- src/theme.tsx
+++ src/theme.tsx
@@ -21,6 +21,9 @@ declare module '@mui/material/Box' {
 
 // A custom theme for this app
 const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
   cssVariables: true,
   palette: {
     // mode: 'dark',
@@ -43,7 +46,7 @@ const theme = createTheme({
         {
           styleOverrides: {
             body: {
-              backgroundColor: "#5D576B",
+              // backgroundColor: "#5D576B",
               backgroundImage: `url("data:image/svg+xml,%3Csvg width='200' height='200' viewBox='0 0 268 268' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23ffffff' fill-opacity='0.08' fill-rule='evenodd'%3E%3Ccircle cx='37' cy='37' r='36'/%3E%3Ccircle cx='171' cy='171' r='36'/%3E%3C/g%3E%3C/svg%3E")`,
               backgroundAttachment: 'fixed',
             }
