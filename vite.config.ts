0388abe79ef9bd15959a848d94cad9898a4f50b5
qwe7002 <git@qwe7002.dev>
2026-01-04 01:13:02 -0800

    feat: update deployment configuration to use GitHub Pages and remove Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/config-generate/",
     plugins: [react(), topLevelAwait()],
     build: {
         target: "esnext",
