242618fbdd95f3ba1653e74b2de78a14a06745fa
qwe7002 <git@qwe7002.dev>
2026-01-04 00:25:44 -0800

    feat: add vite-plugin-top-level-await to enhance async handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), topLevelAwait()],
     build: {
         target: "esnext",
     },
